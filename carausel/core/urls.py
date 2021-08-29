f4017e4a956ecafd2c284dd941e883c7c914771c
H-Cavid <cavid.hasanov2@bk.ru>
2021-08-29 18:28:55 +0400

    <<--Navbar and dynamic Slider is added-->>

--- carausel/core/urls.py
+++ carausel/core/urls.py
@@ -3,7 +3,7 @@ from django.urls.resolvers import URLPattern
 from .views import *
 
 urlpatterns = [
-    path('',HomeView),
+    path('',HomeView,name='home'),
     path('contact/',contact,name='contact'),
     path('about/',about,name='about'),
 ]
